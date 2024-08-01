6aa153ee637459e181e481648aaf09706d038913
bakhromov <najottalim@gmail.com>
2024-08-01 14:03:53 +0500

    Exam successfully

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import BlogDetailView, BlogCommentCreate, BlogListView
+from .views import BlogDetail, BlogCommentCreate, BlogListView
 urlpatterns = [
     path('blogs/', BlogListView.as_view(), name='blog'),
-    path('blog_detail/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog_detail/<slug:slug>', BlogDetail.as_view(), name='blog_detail'),
     path('blog/<int:pk>/comment/', BlogCommentCreate.as_view(), name='blog_comment'),
 
 ]
